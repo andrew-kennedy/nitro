7462b686601e1bceb6e313988c82ebad917070fb
Andrew Kennedy <andrewk36@gmail.com>
2016-12-12 00:19:33 -0800

    support fish_vi_key_bindings in the powerline

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -12,22 +12,40 @@ function fish_right_prompt
     set -g _nitro_status_last $status_copy
 
     if test "$status_copy" -eq 0
-        set duration_glyph " "
+        set duration_glyph ""
     else
         set status_color red
     end
 
     if test "$CMD_DURATION" -gt 250
+        set -l status_string
         if test ! -z "$status_code"
-            echo -sn (set_color $status_color) "($status_code)" (set_color normal)
+            set status_string " ($status_code)"
         end
 
         set -l duration (echo $CMD_DURATION | humanize_duration)
-        echo -sn (set_color $status_color) " ($duration) $duration_glyph" (set_color normal)
+        set status_string "$status_string ($duration) $duration_glyph"
+        segment_right $status_color black $status_string (set_color normal)
 
     else
         if test ! -z "$status_code"
-            echo -sn (set_color $status_color) "$status_code " (set_color normal)
+            segment_right $status_color black " $status_code " (set_color normal)
         end
     end
+
+    # add fish_vi_mode segments
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+      switch $fish_bind_mode
+        case default
+          segment_right brwhite red "[N]"
+        case insert
+          segment_right brwhite green "[I]"
+        case replace-one
+          segment_right brwhite green "[R]"
+        case visual
+          segment_right brwhite magenta "[V]"
+      end
+      set_color normal
+    end
+    segment_close
 end
